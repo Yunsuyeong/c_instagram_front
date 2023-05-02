b5593ef61184bcbd2aa894eec6654324d6b78948
윤수영 <yunsuyeong@yunsuyeong-ui-MacBookAir.local>
2023-05-02 23:34:17 +0900

    2023/05/02(Frontend Settings 2)

---
+++ src/styles.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle, DefaultTheme } from "styled-components";
+import reset from "styled-reset";
+
+export const lightTheme: DefaultTheme = {
+  fontColor: "#2c2c2c",
+  bgColor: "white",
+};
+
+export const darkTheme: DefaultTheme = {
+  fontColor: "white",
+  bgColor: "#2c2c2c",
+};
+
+export const GlobalStyles = createGlobalStyle`
+  ${reset}
+  input {
+      all:unset;
+    }
+    * {
+      box-sizing:border-box;
+    }
+    body {
+        color:rgb(38,38,38);
+        background-color: #FAFAFA;
+        font-size:14px;
+        font-family:'Open Sans', sans-serif;
+    }
+    a {
+      text-decoration: none;
+    }
+`;
